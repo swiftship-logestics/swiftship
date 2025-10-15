caa94df53fd1a7b3ca92c0caa72ec5dd6c7179fc
accesstbilq <accesstbilq@gmail.com>
2025-10-15 19:33:01 +0530

    create fully responsive FAQ page

---
+++ woonuxt_base/app/pages/FAQ.vue
@@ -0,0 +1,27 @@
+//designer-index-FAQ.vue
+<script setup>
+import { useFetch } from '#app'
+import { NuxtLink } from '#components';
+
+const { data, pending, error } = await useFetch('/api/page')
+
+</script>
+
+<template>
+    <div class="main-content bg-white">
+
+        <div class="lg:mt-[-30px]"></div>
+        <FAQSection />
+
+        <GetInTouch />
+
+        
+
+        <!-- Loading state -->
+        <div v-if="pending" class="text-center text-gray-500">Loading...</div>
+        <!-- Error state -->
+        <div v-if="error" class="text-center text-red-500 font-semibold">
+            Error: {{ error.message }}
+        </div>
+    </div>
+</template>
