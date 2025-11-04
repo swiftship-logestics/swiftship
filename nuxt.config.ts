9cfd42d16bcd539528191f2718861d675d4dd823
accesstbilq <accesstbilq@gmail.com>
2025-11-04 20:18:28 +0530

    Update nuxt.config.ts file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
      devProxy: {
       "/graphql": {
         target: "https://admin.swiftshiplogistics.com.au/graphql",
-        changeOrigin: true,
+        changeOrigin: false,
       },
     },
   },
